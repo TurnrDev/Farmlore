01b80102324c5bce8a80c2b59d7bb833d5a146cc
Jay Turner <jaynicholasturner@gmail.com>
2023-07-28 21:10:52 +0100

    Add Canned Baked Beans item

--- src/main/java/dev/turnr/bangers_and_mash/BangersAndMash.java
+++ src/main/java/dev/turnr/bangers_and_mash/BangersAndMash.java
@@ -1,29 +1,30 @@
 package dev.turnr.bangers_and_mash;
 
 import com.mojang.logging.LogUtils;
+import dev.turnr.bangers_and_mash.item.ModItems;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-@Mod("bangers_and_mash")
+@Mod(BangersAndMash.MOD_ID)
 public class BangersAndMash {
-    // Directly reference a slf4j logger
+    public static final String MOD_ID = "bangers_and_mash";
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public BangersAndMash()
-    {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+    public BangersAndMash() {
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModItems.register(eventBus);
+        eventBus.addListener(this::setup);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void setup(final FMLCommonSetupEvent event)
-    {
+    private void setup(final FMLCommonSetupEvent event) {
         // some preinit code
         LOGGER.info("HELLO FROM PREINIT");
         LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
---
+++ src/main/java/dev/turnr/bangers_and_mash/item/ModItems.java
@@ -0,0 +1,19 @@
+package dev.turnr.bangers_and_mash.item;
+
+import dev.turnr.bangers_and_mash.BangersAndMash;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, BangersAndMash.MOD_ID);
+
+    public static final RegistryObject<Item> CANNED_BAKED_BEANS = ITEMS.register("canned_baked_beans", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
