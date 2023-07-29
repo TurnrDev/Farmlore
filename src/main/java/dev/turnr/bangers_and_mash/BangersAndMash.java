f5bf0ebfa911f05a0b3ab1382f63a3843a7cbbc1
Jay Turner <jaynicholasturner@gmail.com>
2023-07-29 20:56:14 +0100

    Remove unused imports

--- src/main/java/dev/turnr/bangers_and_mash/BangersAndMash.java
+++ src/main/java/dev/turnr/bangers_and_mash/BangersAndMash.java
@@ -2,16 +2,12 @@ package dev.turnr.bangers_and_mash;
 
 import com.mojang.logging.LogUtils;
 import dev.turnr.bangers_and_mash.item.ModItems;
-import net.minecraft.data.DataGenerator;
-import net.minecraft.data.recipes.RecipeBuilder;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
 import org.slf4j.Logger;
 
 @Mod(BangersAndMash.MOD_ID)
