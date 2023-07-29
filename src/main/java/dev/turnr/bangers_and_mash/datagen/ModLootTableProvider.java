8828da04cd8e717dae1e659f15a2adc685771529
Jay Turner <jaynicholasturner@gmail.com>
2023-07-29 20:53:16 +0100

    Add boilerplate for ModLootTableProvider and ModBlockLootTables
    - Added ModLootTableProvider class to generate loot tables for the mod
    - Added ModBlockLootTables class to handle block loot tables generation

--- src/main/java/dev/turnr/bangers_and_mash/datagen/ModLootTableProvider.java
+++ src/main/java/dev/turnr/bangers_and_mash/datagen/ModLootTableProvider.java
@@ -1,5 +1,36 @@
 package dev.turnr.bangers_and_mash.datagen;
 
-public class ModLootTableProvider {
+import com.mojang.datafixers.util.Pair;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.loot.LootTableProvider;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.LootTable.Builder;
+import net.minecraft.world.level.storage.loot.LootTables;
+import net.minecraft.world.level.storage.loot.ValidationContext;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSet;
+import org.jetbrains.annotations.NotNull;
 
+public class ModLootTableProvider extends LootTableProvider {
+
+  public ModLootTableProvider(DataGenerator pGenerator) {
+    super(pGenerator);
+  }
+
+  @Override
+  protected void validate(Map<ResourceLocation, LootTable> map,
+      @NotNull ValidationContext validationTracker) {
+    map.forEach((id, table) -> LootTables.validate(validationTracker, id, table));
+  }
+
+  @Override
+  protected List<Pair<Supplier<Consumer<BiConsumer<ResourceLocation, Builder>>>, LootContextParamSet>> getTables() {
+    // https://youtu.be/rGURjv3jH7Y?list=PLKGarocXCE1Hut51TKKqZKqVZtKLZC48x&t=1222
+    throw new UnsupportedOperationException("TODO: ModLootTableProvider#getTables");
+  }
 }
--- src/main/java/dev/turnr/bangers_and_mash/datagen/loot/ModBlockLootTables.java
+++ src/main/java/dev/turnr/bangers_and_mash/datagen/loot/ModBlockLootTables.java
@@ -1,5 +1,19 @@
 package dev.turnr.bangers_and_mash.datagen.loot;
 
-public class ModBlockLootTables {
+import net.minecraft.data.loot.BlockLoot;
+import net.minecraft.world.level.block.Block;
 
+public class ModBlockLootTables extends BlockLoot {
+
+  @Override
+  protected void addTables() {
+    // https://youtu.be/rGURjv3jH7Y?list=PLKGarocXCE1Hut51TKKqZKqVZtKLZC48x&t=1222
+    throw new UnsupportedOperationException("TODO: ModBlockLootTables#addTables");
+  }
+
+  @Override
+  protected Iterable<Block> getKnownBlocks() {
+    // https://youtu.be/rGURjv3jH7Y?list=PLKGarocXCE1Hut51TKKqZKqVZtKLZC48x&t=1222
+    throw new UnsupportedOperationException("TODO: ModBlockLootTables#getKnownBlocks");
+  }
 }
