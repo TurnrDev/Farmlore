bb8ed147dcb2f22157c7c1e9c80c420cc680394e
Jay Turner <jaynicholasturner@gmail.com>
2023-08-06 16:25:48 +0100

    Add new ingredient items: parsley, basil, coriander, mint, rosemary, and thyme.
    - Added JSON files for each new ingredient item.
    - Registered the new ingredient items in the Ingredients class.

--- src/main/java/dev/turnr/bangers_and_mash/items/Ingredients.java
+++ src/main/java/dev/turnr/bangers_and_mash/items/Ingredients.java
@@ -4,16 +4,24 @@ import dev.turnr.bangers_and_mash.BangersAndMash;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 /**
- * Ingredients
- * This class will hold items which serve purely as ingredients for food items.
- * The items will have no nutritional value, and will not be edible on their own.
- * They will be used in recipes to create food items.
+ * Ingredients This class will hold items which serve purely as ingredients for food items. The
+ * items will have no nutritional value, and will not be edible on their own. They will be used in
+ * recipes to create food items.
  */
 public class Ingredients {
+
   public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS,
       BangersAndMash.MOD_ID);
+
+  public static final RegistryObject<Item> PARSLEY = ITEMS.register("parsley", () -> new Item(new Item.Properties()));
+  public static final RegistryObject<Item> BASIL = ITEMS.register("basil", () -> new Item(new Item.Properties()));
+  public static final RegistryObject<Item> CORIANDER = ITEMS.register("coriander", () -> new Item(new Item.Properties()));
+  public static final RegistryObject<Item> MINT = ITEMS.register("mint", () -> new Item(new Item.Properties()));
+  public static final RegistryObject<Item> ROSEMARY = ITEMS.register("rosemary", () -> new Item(new Item.Properties()));
+  public static final RegistryObject<Item> THYME = ITEMS.register("thyme", () -> new Item(new Item.Properties()));
 
   public static void register() {
     ItemRegistry.register(ITEMS);
