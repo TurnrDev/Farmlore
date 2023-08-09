eae3d52204cf146cbda89f0c1175be90c65bef01
Jay Turner <jaynicholasturner@gmail.com>
2023-08-09 21:14:24 +0100

    Add loot table for Thyme block and update ModBlockLootTables class
    This commit adds a new loot table for the Thyme block, which includes drops of Thyme seeds and the thyme ingredient. The loot table also applies a bonus enchantment of Fortune to the Thyme seeds with a probability of 0.5714286. Additionally, the ModBlockLootTables class has been updated to include the dropSelf method for the Food Processor block and to add the new loot table for the Thyme block using createCropDrops.
    
    Note: This commit was composed by AI (model: GPT-3.5-turbo).

--- src/main/java/dev/turnr/bangers_and_mash/datagen/loot/ModBlockLootTables.java
+++ src/main/java/dev/turnr/bangers_and_mash/datagen/loot/ModBlockLootTables.java
@@ -1,8 +1,14 @@
 package dev.turnr.bangers_and_mash.datagen.loot;
 
 import dev.turnr.bangers_and_mash.blocks.BlockRegistry;
+import dev.turnr.bangers_and_mash.blocks.GenericBlocks;
+import dev.turnr.bangers_and_mash.blocks.herbs.HerbBlock;
+import dev.turnr.bangers_and_mash.items.Ingredients;
+import dev.turnr.bangers_and_mash.items.Seeds;
+import net.minecraft.advancements.critereon.StatePropertiesPredicate;
 import net.minecraft.data.loot.BlockLoot;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.storage.loot.predicates.LootItemBlockStatePropertyCondition;
 import net.minecraftforge.registries.RegistryObject;
 import org.jetbrains.annotations.NotNull;
 
@@ -10,9 +16,16 @@ public class ModBlockLootTables extends BlockLoot {
 
   @Override
   protected void addTables() {
-    for (Block block : getKnownBlocks()) {
-      dropSelf(block);  // Lazy loop for dropping self on everything for now.
-    }
+    this.dropSelf(GenericBlocks.FOOD_PROCESSOR.get());
+
+    this.add(GenericBlocks.THYME.get(),
+        createCropDrops(GenericBlocks.THYME.get(), Ingredients.THYME.get(),
+            Seeds.THYME.get(), LootItemBlockStatePropertyCondition
+                .hasBlockStateProperties(GenericBlocks.THYME.get())
+                .setProperties(
+                    StatePropertiesPredicate.Builder.properties()
+                        .hasProperty(HerbBlock.AGE, HerbBlock.MAX_AGE))));
+
   }
 
   @Override
